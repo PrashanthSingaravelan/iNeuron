d248554179a347fdc4d2998fddb86405e264ee21
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-02-14 12:27:23 +0530

    sunday

--- 21.Flask/flask_demo/app.py
+++ 21.Flask/flask_demo/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template('index.html')
